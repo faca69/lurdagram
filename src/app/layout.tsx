483c15e14f6a6c678c1e7c5b59193dd133a81fe0
faca69 <fnikolov123456789@gmail.com>
2024-10-12 00:08:36 +0200

    added sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Lurdagram",
@@ -16,9 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(GeistSans.className, "antialiased bg-black text-white")}
+        className={cn(
+          GeistSans.className,
+          "antialiased bg-black text-white min-h-screen"
+        )}
       >
-        {children}
+        <main className="min-h-screen flex flex-col items-center">
+          <article className="flex flex-row w-full h-screen ">
+            <section className="w-[290px] border-r-[1px] border-[#262626] ">
+              <Sidebar />
+            </section>
+            <section className="w-full p-0  ">{children}</section>
+          </article>
+        </main>
       </body>
     </html>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,93 @@
+import {
+  CirclePlus,
+  Clapperboard,
+  CompassIcon,
+  HomeIcon,
+  MessageCircle,
+  SearchIcon,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export default function Sidebar() {
+  return (
+    <ol className="flex flex-col gap-3 py-2 h-full px-2 ">
+      <li className="my-7">
+        <Link href={"#"} className="px-4 font-bold text-xl">
+          LURDAGRAM
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-3 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <HomeIcon className="size-8" />
+          Home
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-3 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <SearchIcon className="size-8" />
+          Search
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-3 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <CompassIcon className="size-8" />
+          Explore
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-3 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <Clapperboard className="size-8" />
+          Reels
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-3 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <MessageCircle className="size-8" />
+          Messages
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-3 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <CirclePlus className="size-8" />
+          Create
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={"#"}
+          className="flex items-center gap-1 hover:bg-white/10 px-4 py-2 rounded-md font-medium"
+        >
+          <Avatar>
+            <AvatarImage
+              className="w-8 h-8 rounded-full"
+              src="https://github.com/shadcn.png"
+            />
+            <AvatarFallback>🥶</AvatarFallback>
+          </Avatar>
+          Profile
+        </Link>
+      </li>
+    </ol>
+  );
+}
